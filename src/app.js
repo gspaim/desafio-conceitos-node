30607e22feeb791f035ff40a0e942530811272c7
Guilherme Silva Paim <guilhermepaim.ti@gmail.com>
2020-08-31 21:25:57 -0300

    implement get and post

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const express = require('express');
+const cors = require('cors');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -10,23 +9,28 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+app.get('/repositories', (request, response) => {
+  return response.json(repositories);
+});
+
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+
+  return response.json(repository);
+});
+
+app.put('/repositories/:id', (request, response) => {
   // TODO
 });
 
-app.post("/repositories", (request, response) => {
+app.delete('/repositories/:id', (request, response) => {
   // TODO
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.post("/repositories/:id/like", (request, response) => {
+app.post('/repositories/:id/like', (request, response) => {
   // TODO
 });
 
